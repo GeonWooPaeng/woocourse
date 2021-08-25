bcc13de650ce05a4fc73f60511c025779a25fdec
GeonWooPaeng <gwpaeng@naver.com>
2021-08-25 17:52:52 +0900

    feat: MenuBtn html hidden setting

--- 3_subwayMap/src/index.js
+++ 3_subwayMap/src/index.js
@@ -1,10 +1,10 @@
 //$ 사용이유: 함수랑 변수를 구별하기 위함입니다.
 // jquery를 의미한다고 하지만 이러한 이유로 사용한 것이 아닙니다.
 
-import makeMenu from "./menu/makeMenu.js";
+import menuBtnClick from "./menu/menuBtn.js";
 
 export default function subwayMap() {
-  // makeMenu();
+  menuBtnClick();
 }
 
 new subwayMap();
