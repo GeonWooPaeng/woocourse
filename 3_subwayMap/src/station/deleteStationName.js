ece75bb3b49306797f3f812781ba1b77c469ad5a
GeonWooPaeng <gwpaeng@naver.com>
2021-08-27 16:35:52 +0900

    refactor: 삭제 부분 (정확한 값을 삭제 하게 해놨습니다.) 이름을 찾아서 삭제하게 만들었습니다.

--- 3_subwayMap/src/station/deleteStationName.js
+++ 3_subwayMap/src/station/deleteStationName.js
@@ -1,27 +1,41 @@
+import printStationList from "./printStationList.js";
 
 function deleteTableHtml($dataID) {
   let $stationLists = document.querySelectorAll('.station-list > tbody > tr');
 
   $stationLists.forEach(($stationList) => {
-    console.log($stationList);
     if ($stationList.dataset.id === $dataID) {
       $stationList.remove();
     }
   })
 }
+
+function findDeleteStationName($dataID) {
+  let $stationLists = document.querySelectorAll('.station-list > tbody > tr > td');
+  
+  for (let i = 0; i < $stationLists.length; i++) {
+    if ($stationLists[i].dataset.id === $dataID) {
+      return $stationLists[i].innerText;
+    }
+  }
+}
+
+function DeleteStationNameTolocalStorage($dataID) {
+  let $stationNames = JSON.parse(localStorage.getItem('station'));
+  let $stationDeleteName = findDeleteStationName($dataID);
+
+  $stationNames.splice($stationNames.indexOf($stationDeleteName), 1);
+  localStorage.setItem('station', JSON.stringify($stationNames));
+}
+
 export default function deleteStationName() {
   const $stationDeleteBtns = document.querySelectorAll('.station-delete-button');
 
   $stationDeleteBtns.forEach(($stationDeleteBtn) => {
     $stationDeleteBtn.addEventListener('click', () => {
       if (window.confirm('정말로 삭제하시겠습니까?')) {
-          let $stationNames = JSON.parse(localStorage.getItem('station'));
-          // $stationList.deleteRow($stationDeleteBtn.dataset.id + 1);
-          $stationNames.splice($stationDeleteBtn.dataset.id, 1);
-          localStorage.setItem('station', JSON.stringify($stationNames));
-          deleteTableHtml($stationDeleteBtn.dataset.id);
-          console.log(localStorage);
-          console.log();
-        }});
+        DeleteStationNameTolocalStorage($stationDeleteBtn.dataset.id);
+        deleteTableHtml($stationDeleteBtn.dataset.id);
+      }});
     })
   }
