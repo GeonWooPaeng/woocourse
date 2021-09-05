98dc6ffb577269c1fa87b17e5c974c50581c2481
GeonWooPaeng <gwpaeng@naver.com>
2021-09-05 18:54:15 +0900

    refactor: Map 출력 함수 정리

--- 3_subwayMap/src/init.js
+++ 3_subwayMap/src/init.js
@@ -3,45 +3,7 @@ import inputStationName from "./station/inputStation.js";
 import printLineList from "./line/printLineList.js";
 import inputLine from "./line/inputLine.js";
 import makeSectionBtnToEdit from "./section/sectionTitle.js";
-
-
-
-
-
-
-
-
-function printMapList($key, $mapID) {
-  const $mapUls = document.querySelectorAll('.map > ul');
-  
-  $mapUls.forEach( ( $mapUl ) => {
-    if ($mapUl.dataset.mapId == String($mapID)) {
-      let $stations = JSON.parse(localStorage.getItem($key));
-      $stations.forEach( ( $station ) => {
-        $mapUl.insertAdjacentHTML('beforeend',
-                                    `<li>${$station}</li>`);
-      })
-    }
-  })
-}
-
-function printMapTitle() {
-  const $map = document.querySelector('.map');
-  const $keys = Object.keys(localStorage).sort();
-  let $mapID = 0;
-
-  $map.innerHTML = '';
-  if ($keys !== null) {
-    $keys.forEach( ($key) => {
-      if ($key !== 'station') {
-        $map.insertAdjacentHTML('beforeend',
-                                  `<ul data-map-id="${ $mapID }"><h3><strong> ${$key} </strong></h3></ul>`);
-        printMapList($key, $mapID);
-        $mapID += 1;
-        }
-    })
-  }
-}
+import printMapTitle from "./map/printMap.js";
 
 export default function init() {
     printStationList();
