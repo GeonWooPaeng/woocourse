2211a589baebaa457c9b26d91d0926d32f2e4d11
GeonWooPaeng <gwpaeng@naver.com>
2021-10-13 17:40:54 +0900

    feat: inputList()
    list를 만들어서 num1, operation, num2를 넣어서 계산하는 방법 생각

--- 6_calculator/src/js/index.js
+++ 6_calculator/src/js/index.js
@@ -1,20 +1,42 @@
 // https://velog.io/@ywoosang/addEventListener-%EC%BD%9C%EB%B0%B1%ED%95%A8%EC%88%98-%EC%A0%9C%EB%8C%80%EB%A1%9C-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B8%B0
+
+const inputList = (list, $num1, $operation) => {
+  list.push($num1, $operation);
+};
+
 const checkOperation = (e) => {
-  if (e.target.innerHTML === "/") console.log("/");
-  if (e.target.innerHTML === "X") console.log("X");
-  if (e.target.innerHTML === "-") console.log("-");
-  if (e.target.innerHTML === "+") console.log("+");
-  if (e.target.innerHTML === "=") console.log("=");
+  let list = [];
+  let $num1 = document.querySelector("#total").innerHTML;
+  if (e.target.innerHTML === "/") {
+    inputList(list, $num1, e.target.innerHTML);
+    console.log(list);
+    // $num1 = 0;
+    // putNum();
+  }
+  if (e.target.innerHTML === "X") {
+    inputList(list, $num1, e.target.innerHTML);
+    console.log(list);
+  }
+  if (e.target.innerHTML === "-") {
+    inputList(list, $num1, e.target.innerHTML);
+    console.log(list);
+  }
+  if (e.target.innerHTML === "+") {
+    inputList(list, $num1, e.target.innerHTML);
+    console.log(list);
+  }
+  if (e.target.innerHTML === "=") {
+  }
 };
 
 const clickOperation = () => {
-  const $total = document.querySelector("#total");
   const $operations = document.querySelectorAll(".operations");
 
   $operations.forEach(($operation) => {
     $operation.addEventListener("click", (e) => checkOperation(e));
   });
 };
+
 const putNum = () => {
   const $digitsBtn = document.querySelectorAll(".digits > button");
   const $total = document.querySelector("#total");
