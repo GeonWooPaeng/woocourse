f9b6b8599a2a8d96ab81ec2a3f3680c850bc93f0
GeonWooPaeng <gwpaeng@naver.com>
2021-10-13 17:09:45 +0900

    fix: CheckOperation() 콜백 함수 수정

--- 6_calculator/src/js/index.js
+++ 6_calculator/src/js/index.js
@@ -1,4 +1,21 @@
-const getNum = () => {
+// https://velog.io/@ywoosang/addEventListener-%EC%BD%9C%EB%B0%B1%ED%95%A8%EC%88%98-%EC%A0%9C%EB%8C%80%EB%A1%9C-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B8%B0
+const checkOperation = (e) => {
+  if (e.target.innerHTML === "/") console.log("/");
+  if (e.target.innerHTML === "X") console.log("X");
+  if (e.target.innerHTML === "-") console.log("-");
+  if (e.target.innerHTML === "+") console.log("+");
+  if (e.target.innerHTML === "=") console.log("=");
+};
+
+const clickOperation = () => {
+  const $total = document.querySelector("#total");
+  const $operations = document.querySelectorAll(".operations");
+
+  $operations.forEach(($operation) => {
+    $operation.addEventListener("click", (e) => checkOperation(e));
+  });
+};
+const putNum = () => {
   const $digitsBtn = document.querySelectorAll(".digits > button");
   const $total = document.querySelector("#total");
 
@@ -12,7 +29,8 @@ const getNum = () => {
 };
 
 export default function Calculator() {
-  getNum();
+  putNum();
+  clickOperation();
 }
 
 new Calculator();
