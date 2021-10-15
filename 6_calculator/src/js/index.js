407ffef934d70069656f858bcc89a1e64ec86a40
GeonWooPaeng <gwpaeng@naver.com>
2021-10-15 17:32:57 +0900

    refactor: putModifier.js 로 나누기

--- 6_calculator/src/js/index.js
+++ 6_calculator/src/js/index.js
@@ -1,11 +1,7 @@
 // https://velog.io/@ywoosang/addEventListener-%EC%BD%9C%EB%B0%B1%ED%95%A8%EC%88%98-%EC%A0%9C%EB%8C%80%EB%A1%9C-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B8%B0
 import { putNums } from "./putNums.js";
 import { findOperation } from "./utils.js";
-
-const putModifiers = () => {
-  const $total = document.querySelector("#total");
-  $total.innerHTML = "0";
-};
+import { putModifiers } from "./putModifiers.js";
 
 const checkOperations = (operation) => {
   const $total = document.querySelector("#total");
