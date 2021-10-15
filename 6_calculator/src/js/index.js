68c09c03b3b5004572299ffa38bb7c7f5bc8b96d
GeonWooPaeng <gwpaeng@naver.com>
2021-10-15 17:48:14 +0900

    feat: make all Requirments

--- 6_calculator/src/js/index.js
+++ 6_calculator/src/js/index.js
@@ -1,39 +1,7 @@
 // https://velog.io/@ywoosang/addEventListener-%EC%BD%9C%EB%B0%B1%ED%95%A8%EC%88%98-%EC%A0%9C%EB%8C%80%EB%A1%9C-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B8%B0
 import { putNums } from "./putNums.js";
-import { findOperation } from "./utils.js";
 import { putModifiers } from "./putModifiers.js";
-
-const checkOperations = (operation) => {
-  const $total = document.querySelector("#total");
-
-  let cnt = $total.innerHTML.split("").reduce((cnt, element) => {
-    if (isNaN(Number(element))) {
-      cnt += 1;
-    }
-    return cnt;
-  }, 0);
-  if (cnt === 0) {
-    return ($total.innerHTML += operation);
-  }
-};
-
-const getResult = () => {
-  const $total = document.querySelector("#total");
-  let operation = findOperation();
-
-  if (operation === "/") {
-  } else if (operation === "X") {
-  } else if (operation === "-") {
-  } else if (operation === "+") {
-  }
-};
-
-const putOperations = ({ target }) => {
-  if (target.innerHTML === "=") {
-    return getResult();
-  }
-  return checkOperations(target.innerHTML);
-};
+import { putOperations } from "./putOperations.js";
 
 export default function Calculator() {
   const $digits = document.querySelector(".digits");
