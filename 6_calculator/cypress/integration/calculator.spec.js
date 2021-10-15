42cd2de132e6217bdfa24df6442d52f4000389cb
GeonWooPaeng <gwpaeng@naver.com>
2021-10-15 20:30:59 +0900

    feat: 사칙연산 cypress

--- 6_calculator/cypress/integration/calculator.spec.js
+++ 6_calculator/cypress/integration/calculator.spec.js
@@ -17,4 +17,49 @@ describe("Home", () => {
     cy.get(".operations").contains("=").click();
     cy.get("#total").should("have.text", "1998");
   });
+
+  it("999 - 999", () => {
+    cy.get(".digits").contains("9").click();
+    cy.get(".digits").contains("9").click();
+    cy.get(".digits").contains("9").click();
+
+    cy.get(".operations").contains("-").click();
+
+    cy.get(".digits").contains("9").click();
+    cy.get(".digits").contains("9").click();
+    cy.get(".digits").contains("9").click();
+
+    cy.get(".operations").contains("=").click();
+    cy.get("#total").should("have.text", "0");
+  });
+
+  it("999 x 999", () => {
+    cy.get(".digits").contains("9").click();
+    cy.get(".digits").contains("9").click();
+    cy.get(".digits").contains("9").click();
+
+    cy.get(".operations").contains("X").click();
+
+    cy.get(".digits").contains("9").click();
+    cy.get(".digits").contains("9").click();
+    cy.get(".digits").contains("9").click();
+
+    cy.get(".operations").contains("=").click();
+    cy.get("#total").should("have.text", "998001");
+  });
+
+  it("999 / 999", () => {
+    cy.get(".digits").contains("9").click();
+    cy.get(".digits").contains("9").click();
+    cy.get(".digits").contains("9").click();
+
+    cy.get(".operations").contains("/").click();
+
+    cy.get(".digits").contains("9").click();
+    cy.get(".digits").contains("9").click();
+    cy.get(".digits").contains("9").click();
+
+    cy.get(".operations").contains("=").click();
+    cy.get("#total").should("have.text", "1");
+  });
 });
