847b366222eb8879fca5c2732dd004586881c0a4
GeonWooPaeng <gwpaeng@naver.com>
2021-10-15 16:49:21 +0900

    feat: putNums and putOperations

--- 6_calculator/src/js/index.js
+++ 6_calculator/src/js/index.js
@@ -1,58 +1,56 @@
 // https://velog.io/@ywoosang/addEventListener-%EC%BD%9C%EB%B0%B1%ED%95%A8%EC%88%98-%EC%A0%9C%EB%8C%80%EB%A1%9C-%EC%9D%B4%ED%95%B4%ED%95%98%EA%B8%B0
 
-const inputList = (list, $num1, $operation) => {
-  list.push($num1, $operation);
-};
-
-const checkOperation = (e) => {
-  let list = [];
-  let $num1 = document.querySelector("#total").innerHTML;
-  if (e.target.innerHTML === "/") {
-    inputList(list, $num1, e.target.innerHTML);
-    console.log(list);
-    // $num1 = 0;
-    // putNum();
-  }
-  if (e.target.innerHTML === "X") {
-    inputList(list, $num1, e.target.innerHTML);
-    console.log(list);
-  }
-  if (e.target.innerHTML === "-") {
-    inputList(list, $num1, e.target.innerHTML);
-    console.log(list);
-  }
-  if (e.target.innerHTML === "+") {
-    inputList(list, $num1, e.target.innerHTML);
-    console.log(list);
-  }
-  if (e.target.innerHTML === "=") {
-  }
-};
-
-const clickOperation = () => {
-  const $operations = document.querySelectorAll(".operations");
-
-  $operations.forEach(($operation) => {
-    $operation.addEventListener("click", (e) => checkOperation(e));
-  });
-};
-
-const putNum = () => {
-  const $digitsBtn = document.querySelectorAll(".digits > button");
+const checkNums = () => {
+  const operations = ["/", "X", "-", "+"];
   const $total = document.querySelector("#total");
+  const operation = $total.innerHTML
+    .split("")
+    .find((v) => operations.includes(v));
 
-  $digitsBtn.forEach(($digit) => {
-    $digit.addEventListener("click", () => {
-      if ($total.innerHTML === "0") $total.innerHTML = $digit.innerHTML;
-      else if ($total.innerHTML.length < 3)
-        $total.innerHTML += $digit.innerHTML;
-    });
-  });
+  if (isNaN(Number($total.innerHTML)))
+    return $total.innerHTML.split(operation)[1].length < 3;
+  return $total.innerHTML.length < 3;
+};
+
+const putNums = ({ target }) => {
+  const $total = document.querySelector("#total");
+  if ($total.innerHTML === "0") {
+    return ($total.innerHTML = target.innerHTML);
+  }
+  if (checkNums()) {
+    $total.innerHTML += target.innerHTML;
+  }
+};
+
+const putModifiers = () => {
+  const $total = document.querySelector("#total");
+  $total.innerHTML = "0";
+};
+
+const getResult = () => {
+  const operations = ["/", "X", "-", "+"];
+  const $total = document.querySelector("#total");
+  let operation = $total.innerHTML
+    .split("")
+    .find((v) => operations.includes(v));
+};
+
+const putOperations = ({ target }) => {
+  const $total = document.querySelector("#total");
+  if (target.innerHTML === "=") {
+    return getResult();
+  }
+  return ($total.innerHTML += target.innerHTML);
 };
 
 export default function Calculator() {
-  putNum();
-  clickOperation();
+  const $digits = document.querySelector(".digits");
+  const $modifiers = document.querySelector(".modifiers");
+  const $operations = document.querySelector(".operations");
+
+  $digits.addEventListener("click", putNums);
+  $modifiers.addEventListener("click", putModifiers);
+  $operations.addEventListener("click", putOperations);
 }
 
 new Calculator();
